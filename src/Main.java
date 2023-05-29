d63b1d11d313276d2cd3d757b632850586f4bdb0
Andrea Trasviña <alucero_21@alu.uabcs.mx>
2023-05-29 11:33:31 -0700

    Splash y menu

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        Juego juego = new Juego();
-        juego.setVisible(true);
-        juego.iniciar();
+        
+        SplashMenu splash = new SplashMenu();
+        splash.setVisible(true);
     }
 }
