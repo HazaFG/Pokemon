26d8be8afb84af1c3c14146c70858578a34dabcd
Pedro <peva_21@alu.uabcs.mx>
2023-05-29 22:26:57 -0700

    Se juntaron el splash, menú y el juego (mapa jugable)

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame ventana = new JFrame();
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.setTitle("BomberSmite");
 		
-		AdminitradorJuego aJ = new AdminitradorJuego();
-		ventana.setSize(1015, 540);
-		ventana.add(aJ);
-		
-		aJ.iniciarHiloJuego();
-		
-		ventana.setLocationRelativeTo(null);
-		ventana.setVisible(true);
+		SplashMenu splash = new SplashMenu();
+        splash.setVisible(true);
 		
 	}
 }
